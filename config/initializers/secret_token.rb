28eb05b3aa050ca87d7d22053a7b5d77a1494602
william.jiang <william.jiang@move.com>
2013-02-21 12:52:57 -0800

    rails generate rspec:install commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample::Application.config.secret_token = 'e8a44a79acc967e8db00be56e2a0f25f1aecd65c6e2065db1c1db500737e1648f61ce52cdbbd2baa1f7c1f81fe828832c6494733364c88a41675913d3aa07a0b'
