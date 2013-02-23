55178a7636db9003c5a95d9b2d4edc48e71d657a
william.jiang <william.jiang@move.com>
2013-02-22 16:12:51 -0800

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,8 @@
 #
 class User < ActiveRecord::Base
   attr_accessible :name, :email, :password, :password_confirmation
-
+  has_secure_password
+  
   before_save { |user| user.email = email.downcase }
 
   validates :name, presence: true, length: { maximum: 50 }
@@ -20,5 +21,5 @@ class User < ActiveRecord::Base
 
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,17 @@
 Sample::Application.routes.draw do
+  
+  resources :users
+
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
+  
   get "users/new"
 
   #get "static_pages/home"
   #get "static_pages/help"
   #get "static_pages/about"
   #get "static_pages/contact"
-
-  root to: 'static_pages#home'
-
-  match '/signup',  to: 'users#new'
 
   match '/help',    to: 'static_pages#help'
   match '/about',   to: 'static_pages#about'
